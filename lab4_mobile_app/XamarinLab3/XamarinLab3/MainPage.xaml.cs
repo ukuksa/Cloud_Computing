789fac3f0f77fe5500fec6ab46276b5b6f442a10
Renjoyer <donivinik@gmail.com>
2021-04-15 22:43:29 +0200

    lab4_mobile_app
    lab4_mobile_app

---
+++ lab4_mobile_app/XamarinLab3/XamarinLab3/MainPage.xaml.cs
@@ -0,0 +1,99 @@
+﻿
+using PeopleStoreApp.DataContracts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XamarinLab3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        private readonly IPeopleClient client;
+        private Person person = new Person();
+        public MainPage(IPeopleClient client)
+        {
+            InitializeComponent();
+            IntializeEvents();
+            this.client = client;
+        }
+
+        private void IntializeEvents()
+        {
+            btnSave.Clicked += async (object sender, EventArgs e) =>
+            {
+                if (!Validate())
+                {
+                    await DisplayAlert("Validation Error", "First name, last name, phone number and picture are required.", "Ok");
+                    return;
+                }
+
+                try
+                {
+                    await client.AddPersonAsync(person);
+                    await DisplayAlert("Success", "Data has been saved.", "Ok");
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Error", ex.Message, "Ok");
+                }
+
+            };
+
+            btnCamera.Clicked += async (object sender, EventArgs e) =>
+            {
+                var photo = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+                {
+
+                });
+
+                if (photo != null)
+                {
+                    imgPhoto.Source = ImageSource.FromStream(() => photo.GetStream());
+                    byte[] bytes;
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        photo.GetStream().CopyTo(memoryStream);
+                        bytes = memoryStream.ToArray();
+                    }
+
+                    string base64 = Convert.ToBase64String(bytes);
+                    person.PictureBase64 = base64;
+                }
+            };
+
+            entFirstName.TextChanged += (object sender, TextChangedEventArgs e) =>
+            {
+                person.FirstName = e.NewTextValue;
+            };
+
+            entLastName.TextChanged += (object sender, TextChangedEventArgs e) =>
+            {
+                person.LastName = e.NewTextValue;
+            };
+
+            entPhoneNumber.TextChanged += (object sender, TextChangedEventArgs e) =>
+            {
+                person.PhoneNumber = e.NewTextValue;
+            };
+        }
+
+
+        private bool Validate()
+        {
+            return !(string.IsNullOrWhiteSpace(person.FirstName) ||
+                    string.IsNullOrWhiteSpace(person.LastName) ||
+                    string.IsNullOrWhiteSpace(person.PhoneNumber) ||
+                    string.IsNullOrWhiteSpace(person.PictureBase64)
+                );
+        }
+
+    }
+}
---
+++ lab4_mobile_app/XamarinLab3/XamarinLab3/Models.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace XamarinLab3
+{
+    public class Models : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+ 
+        string firstName = "";
+
+        public string FirstName
+        {
+            get => firstName;
+
+            set
+            {
+                if (firstName == value)
+                {
+                    return;
+                }
+                firstName = value;
+                OnPropertyChangedFirst(nameof(firstName));
+                OnPropertyChangedFirst(nameof(DisplayName));
+            }
+
+        }
+
+        public string DisplayName => $"Entered first name: {FirstName}";
+        void OnPropertyChangedFirst(string firstName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(firstName));
+        }
+
+
+        string lastName = "";
+
+        public string LastName
+        {
+            get => lastName;
+
+            set
+            {
+                if (lastName == value)
+                {
+                    return;
+                }
+                lastName = value;
+                OnPropertyChangedLast(nameof(lastName));
+                OnPropertyChangedLast(nameof(DisplayLastName));
+            }
+
+        }
+
+        public string DisplayLastName => $"Entered last name: {LastName}";
+
+        void OnPropertyChangedLast(string lastName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(lastName));
+        }
+
+
+        string phoneNumber = "";
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+
+            set
+            {
+                if (phoneNumber == value)
+                {
+                    return;
+                }
+                phoneNumber = value;
+                OnPropertyChangedPhone(nameof(phoneNumber));
+                OnPropertyChangedPhone(nameof(DisplayPhoneNumber));
+            }
+
+        }
+
+        public string DisplayPhoneNumber => $"Entered first name: {PhoneNumber}";
+        void OnPropertyChangedPhone(string phoneNumber)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(phoneNumber));
+        }
+    }
+}
